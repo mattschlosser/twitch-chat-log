1480391a8381a493182ab8bb9e877022d84e06c3
Matt Schlosser <hello@mattschlosser.me>
2022-03-16 09:27:44 -0600

    fix: default colour

--- index.mjs
+++ index.mjs
@@ -1,4 +1,3 @@
-
 import chalk from 'chalk';
 import tmi from 'tmi.js';
 
@@ -8,7 +7,8 @@ if (process.argv.length !== 3) {
 
 let channel = process.argv[2];
 
-let client = new tmi.client({channels: [channel]});
-client.on('connected', (message) => console.log(message)); 
-client.on('message', (_, {username, color}, msg) => console.log(chalk.hex(color)(username), msg)); 
-client.on('error', console.log); client.connect()
+let client = new tmi.client({ channels: [channel] });
+client.on('connected', (message) => console.log(message));
+client.on('message', (_, { username, color }, msg) => console.log(chalk.hex(color || '#ffffff')(username), msg));
+client.on('error', console.log);
+client.connect()
