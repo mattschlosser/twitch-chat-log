1250342c540c17d7324349caa7872cf9456ea63b
Matt Schlosser <hello@mattschlosser.me>
2022-03-16 09:08:20 -0600

    Init commit

---
+++ index.mjs
@@ -0,0 +1,14 @@
+
+import chalk from 'chalk';
+import tmi from 'tmi.js';
+
+if (process.argv.length !== 3) {
+    console.error(chalk.red('Usage: node index.mjs <channel>'));
+}
+
+let channel = process.argv[2];
+
+let client = new tmi.client({channels: [channel]});
+client.on('connected', (message) => console.log(message)); 
+client.on('message', (_, {username, color}, msg) => console.log(chalk.hex(color)(username), msg)); 
+client.on('error', console.log); client.connect()
